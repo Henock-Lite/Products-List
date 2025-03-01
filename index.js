bf2f8832823f7f8d02d3d600b8788c7ac875f82a
Henock-Lite <litehenock@gmail.com>
2025-03-01 10:16:43 +0100

    addcart function

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-let cardsContenaire = document.querySelector(".cards-contenaire");
+const cardsContenaire = document.querySelector(".cards-contenaire");
+const cartcontenaireListItem = document.querySelector(".cart-contenaire");
+const listItems = document.querySelector(".list-items");
 
 async function fecthMeal() {
   const res = await fetch("data.json");
   Data = await res.json();
   displayfecth();
-  console.log(Data);
   ButtonInnit();
+  console.log(Data);
+  addcart();
 }
 
 function displayfecth() {
   cardsContenaire.innerHTML = Data.map((data, index) => {
     return `
-  <div class= 'cards-foods'>
+  <div class= 'cards-foods' id=${index}>
     <img src= "${data.image.desktop}" class="imgItem" data-img=${index}>
   <div class= "parent-btn">
     <button class="btn-add" data-btn=${index}>
@@ -20,7 +23,7 @@ function displayfecth() {
     <span>Add to Cart</span>
      </button>
     </div>
-    <div class="down-liste">
+    <div class="down-liste" >
        <p>${data.category}</p>
        <b>${data.name}</b>
        <p class ="price">${data.price}$</p>
@@ -31,10 +34,25 @@ function displayfecth() {
   }).join("");
 }
 
+function addcart(btnaddValue) {
+  const AllFoodscards = document.querySelectorAll(".cards-foods");
+  const cardOrder = document.querySelector(".card-order");
+  const ParentlistItems = document.querySelector(".List-ItemsParent");
+  const ListItem = document.createElement("li");
+  const Listchild = ParentlistItems.appendChild(ListItem);
+  const downliste = document.querySelector(".down-liste");
+
+  AllFoodscards.forEach((cardsFoods, index) => {
+    if (cardsFoods.id == btnaddValue) {
+      Listchild.textContent = `${Data[index].name}`;
+    }
+  });
+}
+
 function ButtonInnit() {
   let buttonInnerhtml = `
    <div class="Elementbtn-increment">
-           <div class="parent-icon" id= "iconincrementMoin" data-moin="moin">
+           <div class="parent-icon" id= "iconDecrementMoin" data-moin="moin">
              <img
                 src="./assets/images/icon-decrement-quantity.svg"
               />
@@ -53,47 +71,40 @@ function ButtonInnit() {
     if (e.target.closest(".btn-add")) {
       const btnAdd = e.target.closest(".btn-add");
       const index = btnAdd.dataset.btn;
-      let button = document.createElement("button");
-      let Imgborder = document.querySelector(`.imgItem[data-img="${index}"]`);
-
-      // console.log(index);
+      const button = document.createElement("button");
+      const Imgborder = document.querySelector(`.imgItem[data-img="${index}"]`);
 
       button.setAttribute("data-button", `"${index}"`);
       button.innerHTML = buttonInnerhtml;
       button.classList.add("btn-increment");
       btnAdd.parentNode.replaceChild(button, btnAdd);
-      
       if (Imgborder) {
         Imgborder.style.border = "solid 3px hsl(16, 69%, 49%)";
       }
-    
-      // let moin = document.querySelectorAll(`[data-moin="moin"]`);
-      
-      // moin.forEach((moin) => {
-        //   moin.addEventListener("click", (e) => {
-          //     let parent = e.target.closest(".Elementbtn-increment");
-      //     let displaydecrement = parent.querySelector("#displaynumber");
-      //     if (displaydecrement.textContent != 0) {
-      //       let decrementation = displaydecrement.textContent;
-      //       decrementation--;
-      //       displaydecrement.textContent = `${decrementation}`;
-      //     }
-      //   });
-      // });
+      addcart(index);
     }
   });
-
   cardsContenaire.addEventListener("click", (e) => {
     if (e.target.closest(`[data-plus="plus"]`)) {
-      let btnPlus = e.target.closest(`[data-plus="plus"]`);
-      let parent = btnPlus.closest(".Elementbtn-increment");
-      let displayincrement = parent.querySelector("#displaynumber");
+      const btnPlus = e.target.closest(`[data-plus="plus"]`);
+      const parent = btnPlus.closest(".Elementbtn-increment");
+      const displayincrement = parent.querySelector("#displaynumber");
       let increment = parseInt(displayincrement.textContent);
       increment++;
       displayincrement.textContent = `${increment}`;
     }
-  });
 
+    if (e.target.closest(`[data-moin="moin"]`)) {
+      const btnMoin = e.target.closest(`[data-moin="moin"]`);
+      const parent = btnMoin.closest(".Elementbtn-increment");
+      const displayincrement = parent.querySelector("#displaynumber");
+      if (displayincrement.textContent != 1) {
+        let decrementation = parseInt(displayincrement.textContent);
+        decrementation--;
+        displayincrement.textContent = `${decrementation}`;
+      }
+    }
+  });
 }
 
 window.addEventListener("load", fecthMeal);
